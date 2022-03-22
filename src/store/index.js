d2b6be9c0e743ef1a53b8ab3e1c70b6cb4fd6f01
Yves Faems <yves.faems@gmail.com>
2022-03-21 19:17:10 -0700

    create store + counterReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,23 @@
 const redux = require('redux');
+
+const store = redux.createStore(counterReducer);
+
+const counterReducer = (state = {
+    counter: 0
+}, action) => {
+    if ( action.type === 'increment') {
+        counter: state.counter + 1;
+    };
+    if ( action.type === 'decrement') {
+        counter: state.counter - 1;
+    };
+    return {
+        counter: 0
+    }
+}
+
+
+
+console.log(store.dispatch({
+    type: 'increment'
+}));
