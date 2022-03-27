3d6e3cf1dca8828dc6292b2d7ee22fc64b8c3b27
Yves Faems <yves.faems@gmail.com>
2022-03-26 17:16:43 -0700

    refactoring code by use of redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import store from '../store';
 import classes from './Counter.module.css';
 import {useSelector, useDispatch} from 'react-redux';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
     const dispatch = useDispatch();
@@ -12,18 +13,16 @@ const Counter = () => {
             );
 
     const incrementHandler = () => {
-        dispatch({type: 'increment'})
+        dispatch(counterActions.increment())
     }
     const increaseHandler = () => {
-        dispatch({
-            type: 'increase',
-            amount: 5})
+        dispatch(counterActions.increase(5))
     }
     const decrementHandler = () => {
-        dispatch({type: 'decrement'})
+        dispatch(counterActions.decrement())
     }
   const toggleCounterHandler = () => {
-        dispatch({type: 'toggle'})
+        dispatch(counterActions.toggle())
   };
 
   return (
